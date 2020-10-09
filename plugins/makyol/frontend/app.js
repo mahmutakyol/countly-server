d35b1bfabbe9eedbfb2036c3c024584672a276a1
mahmutakyol <mahmut.akyol@outlook.com.tr>
2020-10-10 01:33:29 +0300

    fixed app.js

--- plugins/makyol/frontend/app.js
+++ plugins/makyol/frontend/app.js
@@ -1,42 +1,9 @@
-var plugin = {},
-    countlyConfig = require('../../../frontend/express/confg');
-
+var exported = {};
 
 (function(plugin) {
-    plugin.init = function(app, countlyDb, express) {
+    plugin.init = function() {
 
-        //middleware or process request
-        app.get(countlyConfig.path + '/makyol', function(req, res, next) {
-
-            //url parameters
-            var parts = req.url.split("/");
-            var id = parts[parts.length - 1];
-
-            // read data fro db using countlyDb
-            countlyDb.collection('makyol').findOne({
-                '_id': id
-            }, function(err, plugindata) {
-
-                //if data is null
-                if (err || !plugindata) res.send('404: Page Not Found', 404);
-                else {
-
-                    //template rendering
-                    res.render('../../../plugins/makyol/frontend/public/teplates/makyol', {
-                        path: countlyConfig.path || "",
-                        cdn: countlyConfig.cdn || "",
-                        data: plugindata
-                    });
-                }
-            });
-
-            next();
-        });
     };
-}(plugin));
+}(exported));
 
-plugin.init = function(/*app, countlyDb*/) {
-    //add middleware here
-};
-
-module.exports = plugin;
+module.exports = exported;
